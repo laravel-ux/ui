84a22d3fb34ff28377db881ff57a7595a8784dae
Oleksandr Kokoriev <akokorev@lenderkit.com>
2025-02-21 20:18:31 +0200

    init

---
+++ src/SupportsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaravelUi\Supports;
+
+use Illuminate\Support\ServiceProvider;
+
+class SupportsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
