b9a7e58df28988f56d5cc6c1d0240e9650cf07e0
Oleksandr Kokoriev <akokorev@lenderkit.com>
2026-08-12 21:36:44 +0300

    fix: formatting via pint

--- resources/views/components/sidebar/group/content.blade.php
+++ resources/views/components/sidebar/group/content.blade.php
@@ -1,7 +1,3 @@
-<div
-    data-slot="sidebar-group-content"
-    data-sidebar="group-content"
-    {{ $attributes->tailwindMerge('w-full text-sm') }}
->
+<div data-slot="sidebar-group-content" data-sidebar="group-content" {{ $attributes->tailwindMerge('w-full text-sm') }}>
     {{ $slot }}
 </div>
